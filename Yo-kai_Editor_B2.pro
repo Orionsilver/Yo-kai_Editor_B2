d1ea8fd9b700ccb03658f273b92873289187fa2a
togenyan <nyantoge@gmail.com>
2017-12-18 02:44:45 +0900

    initial commit

---
+++ Yo-kai_Editor_B2.pro
@@ -0,0 +1,176 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-03-15T09:45:07
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = "Yo-kai Editor B2"
+TEMPLATE = app
+
+CONFIG += c++11
+
+INCLUDEPATH += src
+
+SOURCES += src/main.cpp\
+        src/mainwindow.cpp \
+    src/version_dialog.cpp \
+    src/savemanager.cpp \
+    src/xorshift.cpp \
+    src/ywcipher.cpp \
+    src/ccmcipher.cpp \
+    src/crc32.cpp \
+    src/error.cpp \
+    src/tab.cpp \
+    src/listtab.cpp \
+    src/youkaitab.cpp \
+    src/gamedata.cpp \
+    src/gameconfig.cpp \
+    src/manualeditdialog.cpp \
+    src/QHexEdit/qhexedit.cpp \
+    src/QHexEdit/qhexeditcomments.cpp \
+    src/QHexEdit/qhexeditdata.cpp \
+    src/QHexEdit/qhexeditdatadevice.cpp \
+    src/QHexEdit/qhexeditdatareader.cpp \
+    src/QHexEdit/qhexeditdatawriter.cpp \
+    src/QHexEdit/qhexedithighlighter.cpp \
+    src/QHexEdit/qhexeditprivate.cpp \
+    src/QHexEdit/sparserangemap.cpp \
+    src/itemtab.cpp \
+    src/equipmenttab.cpp \
+    src/importanttab.cpp \
+    src/section1tab.cpp \
+    src/section.cpp \
+    src/section9tab.cpp \
+    src/medaliumtab.cpp \
+    src/youkaichecklist.cpp \
+    src/section242tab.cpp \
+    src/hackslashtab.cpp \
+    src/gashatab.cpp \
+    src/gashalot.cpp \
+    src/checklist.cpp \
+    src/flagchecklist.cpp \
+    src/trophychecklist.cpp \
+    src/cheatcodedialog.cpp \
+    src/cybercode/AddCode.cpp \
+    src/cybercode/CyberCode.cpp \
+    src/cybercode/CyberCodeParser.cpp \
+    src/cybercode/LoopCode.cpp \
+    src/cybercode/OffsetCode.cpp \
+    src/cybercode/SearchCode.cpp \
+    src/cybercode/SectionWriteCode.cpp \
+    src/cybercode/WriteCode.cpp \
+    src/busters_equipmenttab.cpp \
+    src/busters_itemtab.cpp \
+    src/busters_hiddentreasuretab.cpp \
+    src/cybercode/SectionOffsetCode.cpp \
+    src/dataeditor/dataeditor.cpp \
+    src/dataeditor/integereditor.cpp \
+    src/dataeditor/biteditor.cpp \
+    src/dataeditor/stringeditor.cpp \
+    src/dataeditor/listeditor.cpp \
+    src/dataeditor/hexintegereditor.cpp \
+    src/gashastate.cpp
+
+HEADERS  += src/mainwindow.h \
+    src/version.h \
+    src/version_dialog.h \
+    src/savemanager.h \
+    src/gameconfig.h \
+    src/xorshift.h \
+    src/ywcipher.h \
+    src/ccmcipher.h \
+    src/crc32.h \
+    src/error.h \
+    src/tab.h \
+    src/listtab.h \
+    src/youkaitab.h \
+    src/gamedata.h \
+    src/manualeditdialog.h \
+    src/QHexEdit/qhexedit.h \
+    src/QHexEdit/qhexeditcomments.h \
+    src/QHexEdit/qhexeditdata.h \
+    src/QHexEdit/qhexeditdatadevice.h \
+    src/QHexEdit/qhexeditdatareader.h \
+    src/QHexEdit/qhexeditdatawriter.h \
+    src/QHexEdit/qhexedithighlighter.h \
+    src/QHexEdit/qhexeditprivate.h \
+    src/QHexEdit/sparserangemap.h \
+    src/itemtab.h \
+    src/equipmenttab.h \
+    src/importanttab.h \
+    src/section1tab.h \
+    src/section.h \
+    src/section9tab.h \
+    src/medaliumtab.h \
+    src/youkaichecklist.h \
+    src/section242tab.h \
+    src/hackslashtab.h \
+    src/gashatab.h \
+    src/gashalot.h \
+    src/checklist.h \
+    src/flagchecklist.h \
+    src/trophychecklist.h \
+    src/cheatcodedialog.h \
+    src/cybercode/AddCode.h \
+    src/cybercode/CyberCode.h \
+    src/cybercode/CyberCodeParser.h \
+    src/cybercode/LoopCode.h \
+    src/cybercode/OffsetCode.h \
+    src/cybercode/SearchCode.h \
+    src/cybercode/SectionWriteCode.h \
+    src/cybercode/WriteCode.h \
+    src/busters_equipmenttab.h \
+    src/busters_itemtab.h \
+    src/busters_hiddentreasuretab.h \
+    src/cybercode/SectionOffsetCode.h \
+    src/dataeditor/dataeditor.h \
+    src/dataeditor/integereditor.h \
+    src/dataeditor/biteditor.h \
+    src/dataeditor/stringeditor.h \
+    src/dataeditor/listeditor.h \
+    src/dataeditor/hexintegereditor.h \
+    src/gashastate.h
+
+FORMS    += src/ui/mainwindow.ui \
+    src/ui/version_dialog.ui \
+    src/ui/manualeditdialog.ui \
+    src/ui/section1tab.ui \
+    src/ui/section9tab.ui \
+    src/ui/medaliumtab.ui \
+    src/ui/listtab.ui \
+    src/ui/youkaitabform.ui \
+    src/ui/itemtabform.ui \
+    src/ui/equipmenttabform.ui \
+    src/ui/importanttabform.ui \
+    src/ui/section242tab.ui \
+    src/ui/hackslashtabform.ui \
+    src/ui/gashatab.ui \
+    src/ui/cheatcodedialog.ui \
+    src/ui/busters_equipmenttabform.ui \
+    src/ui/busters_itemtabform.ui \
+    src/ui/busters_hiddentreasuretabform.ui
+
+TRANSLATIONS = $$PWD/translations/app_ja.ts \
+$$PWD/translations/app_en.ts
+
+macx {
+APPBUNDLE = "Yo-kai Editor B2.app"
+ICON = icon.icns
+LIBS += -L$$PWD/../../local/lib -lcryptopp
+INCLUDEPATH += $$PWD/../../local/include
+QMAKE_CXXFLAGS_WARN_ON += -Wno-unknown-pragmas
+QMAKE_TARGET_BUNDLE_PREFIX = jp.togenyan
+}
+
+win32:LIBS += -L$$PWD/../lib/ -lcryptlib
+
+win32:INCLUDEPATH += $$PWD/../include
+win32:DEPENDPATH += $$PWD/../include
+
+RESOURCES += \
+    resources.qrc
+RC_FILE = app.rc
---
+++ src/mainwindow.cpp
@@ -0,0 +1,227 @@
+#pragma execution_character_set("utf-8")
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QIcon>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , mgr(new SaveManager)
+    , manualEdit(new ManualEditDialog(this, mgr))
+    , cheatEdit(new CheatCodeDialog(this, mgr))
+{
+    QIcon icon(":/appInfo/small_icon.png");
+    ui->setupUi(this);
+    this->setWindowIcon(icon);
+    ui->version_label->setText(QString(VERSION_STRING));
+    connect(ui->actionVersion_information, SIGNAL(triggered()), SLOT(showVersionDialog()));
+    connect(ui->actionOpen, SIGNAL(triggered()), SLOT(openFile()));
+    connect(ui->actionSave, SIGNAL(triggered()), SLOT(saveFile()));
+    connect(ui->actionOpenDecrypted, SIGNAL(triggered()), SLOT(openDecryptedFile()));
+    connect(ui->actionSaveDecrypted, SIGNAL(triggered()), SLOT(saveDecryptedFile()));
+    connect(ui->actionManual_Edit, SIGNAL(triggered(bool)), SLOT(showHexEditor()));
+    connect(ui->actionCheatCode_Edit, SIGNAL(triggered(bool)), SLOT(showCheatEditor()));
+
+    this->setAcceptDrops(true);
+
+    this->manualEdit->setTreeWidget(this->mgr->getTw());
+
+    YoukaiTab* yt = new YoukaiTab(this->mgr, this, 0x07);
+    Section1Tab* s1t = new Section1Tab(this->mgr, this, 0x01);
+    //    Section242Tab* s242t = new Section242Tab(this->mgr, this, 0xF2);
+    //    GashaTab* gt = new GashaTab(this->mgr, this, 0x0E);
+    ItemTab* it = new ItemTab(this->mgr, this, 0x04);
+    EquipmentTab* et = new EquipmentTab(this->mgr, this, 0x05);
+    ImportantTab* imt = new ImportantTab(this->mgr, this, 0x06);
+    MedaliumTab* mt = new MedaliumTab(this->mgr, this, 0x01, 0x774);
+    HackSlashTab* ht = new HackSlashTab(this->mgr, this, 0x27); // 0x27 for item box, 0x28 for backpack items
+    Section9Tab* s9t = new Section9Tab(this->mgr, this, 0x09);
+
+    this->ui->tabWidget->addTab(yt, tr("YOKAI"));
+    this->ui->tabWidget->addTab(s1t, tr("INFO1"));
+    //    this->ui->tabWidget->addTab(s242t, tr("INFO2"));
+    //    this->ui->tabWidget->addTab(gt, tr("CRANK_A_KAI"));
+    this->ui->tabWidget->addTab(it, tr("ITEM"));
+    this->ui->tabWidget->addTab(et, tr("EQUIPMENT"));
+    this->ui->tabWidget->addTab(imt, tr("IMPORTANT"));
+    this->ui->tabWidget->addTab(mt, tr("MEDALLIUM"));
+    this->ui->tabWidget->addTab(ht, tr("HACKSLASH_BATTLE"));
+    this->ui->tabWidget->addTab(s9t, tr("INFO"));
+
+    this->sectionTabs.append(yt);
+    this->sectionTabs.append(it);
+    this->sectionTabs.append(et);
+    this->sectionTabs.append(imt);
+    this->sectionTabs.append(mt);
+    this->sectionTabs.append(ht);
+    this->sectionTabs.append(s1t);
+    this->sectionTabs.append(s9t);
+    //    this->sectionTabs.append(s242t);
+    //    this->sectionTabs.append(gt);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete mgr;
+}
+
+Error::ErrorCode MainWindow::openFileWithError(const QString& file, bool encrypted)
+{
+    if (file.isEmpty()) {
+        return Error::IO_ERROR;
+    }
+    Error::ErrorCode status;
+
+    if (encrypted) {
+        status = this->mgr->loadFile(file);
+    } else {
+        status = this->mgr->loadDecryptedFile(file);
+    }
+    if (this->mgr->loaded()) {
+        ui->actionSave->setEnabled(true);
+        ui->actionSaveDecrypted->setEnabled(true);
+        ui->actionManual_Edit->setEnabled(true);
+        ui->actionCheatCode_Edit->setEnabled(true);
+        this->updateTabs();
+    }
+    if (status == Error::SUCCESS) {
+        this->setWindowTitle(this->mgr->getFilepath() + " - Yo-kai Editor B2");
+    }
+    return status;
+}
+
+void MainWindow::openFileWithPath(const QString& file, bool encrypted)
+{
+    Error::ErrorCode status;
+    if ((status = openFileWithError(file, encrypted)) != Error::SUCCESS) {
+        QMessageBox::critical(this, tr("ERROR"), QString(tr("ERROR (%1)")).arg(status));
+    }
+}
+
+void MainWindow::saveFileWithPath(const QString& file, bool encrypted)
+{
+    if (file.isEmpty()) {
+        return;
+    }
+    Error::ErrorCode status;
+    if (encrypted) {
+        status = this->mgr->writeFile(file);
+    } else {
+        status = this->mgr->writeDecryptedFile(file);
+    }
+    if (status != Error::SUCCESS) {
+        QMessageBox::critical(this, tr("ERROR"), QString(tr("ERROR (%1)")).arg(status));
+    }
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent* e)
+{
+    e->acceptProposedAction();
+}
+
+void MainWindow::dropEvent(QDropEvent* e)
+{
+    Error::ErrorCode s1, s2;
+    if (e->mimeData()->hasUrls()) {
+        QString path = e->mimeData()->urls().first().toLocalFile();
+        if (path.endsWith(".ywd")) {
+            this->openFileWithPath(path, false);
+        } else {
+            if ((s1 = this->openFileWithError(path, true)) != Error::SUCCESS
+                && (s2 = this->openFileWithError(path, false)) != Error::SUCCESS) {
+                /* 暗号化済み・復号済みとみなしてもともに失敗した */
+                QMessageBox::critical(this, tr("ERROR"), QString(tr("ERROR (%1-%2)")).arg(s1).arg(s2));
+                return;
+            }
+        }
+    }
+}
+
+void MainWindow::showVersionDialog()
+{
+    Version_Dialog* dlg;
+    dlg = new Version_Dialog(this);
+    dlg->exec();
+}
+
+void MainWindow::openFile(bool encrypted)
+{
+    QString file;
+    QString filter;
+
+    if (encrypted) {
+        filter = QString(tr("YO-KAI_WATCH_SAVEDATA (*.yw)"));
+    } else {
+        filter = QString(tr("YO-KAI_WATCH_DECRYPTED_SAVEDATA (*.ywd *.yw)"));
+    }
+    file = QFileDialog::getOpenFileName(this, tr("SELECT_FILE_TO_OPEN"),
+        this->mgr->getFilepath(), filter);
+    if (file.isEmpty()) {
+        return;
+    }
+    openFileWithPath(file, encrypted);
+}
+
+void MainWindow::saveFile(bool encrypted)
+{
+    QString file;
+    QString filter;
+
+    if (this->mgr->loaded()) {
+        if (encrypted) {
+            filter = QString(tr("YO-KAI_WATCH_SAVEDATA (*.yw)"));
+        } else {
+            filter = QString(tr("YO-KAI_WATCH_DECRYPTED_SAVEDATA (*.ywd *.yw)"));
+        }
+        file = QFileDialog::getSaveFileName(this, tr("INPUT_FILENAME"),
+            this->mgr->getFilepath(), filter);
+
+        if (file.isEmpty()) {
+            return;
+        }
+        saveFileWithPath(file, encrypted);
+    }
+}
+
+void MainWindow::openDecryptedFile()
+{
+    this->openFile(false);
+}
+
+void MainWindow::saveDecryptedFile()
+{
+    this->saveFile(false);
+}
+
+void MainWindow::showHexEditor()
+{
+    this->setAcceptDrops(false);
+    int st = this->manualEdit->exec();
+    if (st == QDialog::Accepted) {
+        this->updateTabs();
+    }
+    this->setAcceptDrops(true);
+}
+
+void MainWindow::showCheatEditor()
+{
+    this->setAcceptDrops(false);
+    this->cheatEdit->exec();
+    if (this->cheatEdit->getIsDirty()) {
+        this->updateTabs();
+    }
+    this->setAcceptDrops(true);
+}
+
+void MainWindow::updateTabs()
+{
+    for (QList<Tab*>::const_iterator it = this->sectionTabs.constBegin();
+         it != this->sectionTabs.constEnd();
+         ++it) {
+        (*it)->update();
+    }
+}
